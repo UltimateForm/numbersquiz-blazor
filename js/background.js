24cb754ee142cf0807cb11637a8424253000083a
helio <heliojmsemedo@gmail.com>
2020-08-20 12:25:14 +0100

    files

---
+++ js/background.js
@@ -0,0 +1,133 @@
+﻿import * as THREE from 'https://unpkg.com/three@0.118.3/build/three.module.js';
+import { OrbitControls } from "https://unpkg.com/three@0.118.3/examples/jsm/controls/OrbitControls.js";
+
+var container;
+var camera, controls, scene, renderer, windowHalfX, objects;
+
+function init() {
+	objects = [];
+
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+
+	// camera
+
+	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 10000);
+	camera.position.z = 1500;
+
+	// renderer
+
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(width, height);
+	renderer.outputEncoding = THREE.sRGBEncoding;
+
+	container = document.getElementById("background_container");
+	container.appendChild(renderer.domElement);
+
+	// scene
+
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color("#222831");
+
+	// controls
+	controls = new OrbitControls(camera, renderer.domElement);
+	controls.autoRotate = true;
+	controls.enableZoom = false;
+	controls.enablePan = false;
+
+
+	// listeners
+
+	window.addEventListener('resize', onWindowResize, false);
+
+	Object.assign(window, { scene });
+
+}
+
+function createMeshes() {
+	var loader = new THREE.FontLoader();
+	loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+
+		/* 		var geometry = new THREE.TextGeometry('1', {
+					font: font,
+					size: 1,
+					height: 0.5,
+					curveSegments: 4,
+					bevelEnabled: true,
+					bevelThickness: 0.02,
+					bevelSize: 0.05,
+					bevelSegments: 3
+				});
+				geometry.computeVertexNormals();
+		
+				var material = new THREE.MeshNormalMaterial();
+				var mesh = new THREE.Mesh(geometry, material);
+				mesh.position.x = Math.random() * 8000 - 4000;
+				mesh.position.y = Math.random() * 8000 - 4000;
+				mesh.position.z = Math.random() * 8000 - 4000;
+				mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 500 + 1000;
+				mesh.rotation.x = Math.random() * 2 * Math.PI;
+				mesh.rotation.y = Math.random() * 2 * Math.PI;
+				scene.add(mesh);
+						return;
+		
+				*/
+		for (var i = 0; i < 250; i++) {
+			var n = Math.floor(Math.random() * (10 - 1)) + 1;
+			var geometry = new THREE.TextGeometry(n.toString(), {
+				font: font,
+				size: 1,
+				height: 0.5,
+				curveSegments: 4,
+				bevelEnabled: true,
+				bevelThickness: 0.02,
+				bevelSize: 0.05,
+				bevelSegments: 3
+			});
+			geometry.computeVertexNormals();
+			var material = new THREE.MeshNormalMaterial();
+			var mesh = new THREE.Mesh(geometry, material);
+
+			mesh.position.x = Math.random() * 6000 - 3000; //these positions form a square space, remember to clamp to magnitude
+			mesh.position.y = Math.random() * 6000 - 3000;
+			mesh.position.z = Math.random() * 6000 - 3000;
+			mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 50 + 100;
+			mesh.rotation.x = Math.random() * 2 * Math.PI;
+			mesh.rotation.y = Math.random() * 2 * Math.PI;
+
+			objects.push(mesh);
+			scene.add(mesh);
+		}
+
+	});
+}
+
+function onWindowResize() {
+
+	windowHalfX = window.innerWidth / 2;
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	controls.update()
+	render();
+}
+
+function render() {
+	for (var i = 0, il = objects.length; i < il; i++) {
+		objects[i].rotation.x += 0.01;
+		objects[i].rotation.y += 0.02;
+	}
+	renderer.render(scene, camera);
+}
+
+init();
+createMeshes();
+animate();
---
+++ service-worker-assets.js
@@ -0,0 +1,209 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-AqlUgi3paUehHoanG3O3imGRZ0nq7n\/vtKj14t6NWVg=",
+      "url": "appsettings.json"
+    },
+    {
+      "hash": "sha256-BHNhq10T\/9aejlrFEhS\/NYeYS3Djwv\/pqg2xUjQCx\/s=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-rldnE7wZYJj3Q43t5v8fg1ojKRwyt0Wtfm+224CacZs=",
+      "url": "css\/bootstrap\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-xMZ0SaSBYZSHVjFdZTAT\/IjRExRIxSriWcJLcA9nkj0=",
+      "url": "css\/bootstrap\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-W7JhGjvSxZDrykJ9W6hDP9szw1eOLOQ6ednQ\/3pfN00=",
+      "url": "css\/index.css"
+    },
+    {
+      "hash": "sha256-jA4J4h\/k76zVxbFKEaWwFKJccmO0voOQ1DbUW+5YNlI=",
+      "url": "css\/open-iconic\/FONT-LICENSE"
+    },
+    {
+      "hash": "sha256-BJ\/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=",
+      "url": "css\/open-iconic\/font\/css\/open-iconic-bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-OK3poGPgzKI2NzNgP07XMbJa3Dz6USoUh\/chSkSvQpc=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.eot"
+    },
+    {
+      "hash": "sha256-sDUtuZAEzWZyv\/U1xl\/9D3ehyU69JE+FvAcu5HQ+\/a0=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.otf"
+    },
+    {
+      "hash": "sha256-+P1oQ5jPzOVJGC52E1oxGXIXxxCyMlqy6A9cNxGYzVk=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.svg"
+    },
+    {
+      "hash": "sha256-p+RP8CV3vRK1YbIkNzq3rPo1jyETPnR07ULb+HVYL8w=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.ttf"
+    },
+    {
+      "hash": "sha256-cZPqVlRJfSNW0KaQ4+UPOXZ\/v\/QzXlejRDwUNdZIofI=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.woff"
+    },
+    {
+      "hash": "sha256-aF5g\/izareSj02F3MPSoTGNbcMBl9nmZKDe04zjU\/ss=",
+      "url": "css\/open-iconic\/ICON-LICENSE"
+    },
+    {
+      "hash": "sha256-p\/oxU91iBE+uaDr3kYOyZPuulf4YcPAMNIz6PRA\/tb0=",
+      "url": "css\/open-iconic\/README.md"
+    },
+    {
+      "hash": "sha256-qU+KhVPK6oQw3UyjzAHU4xjRmCj3TLZUU\/+39dni9E0=",
+      "url": "favicon.ico"
+    },
+    {
+      "hash": "sha256-IpHFcQvs03lh6W54zoeJRG1jW+VnGmwymzxLoXtKX7Y=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-UT3Q0+lc1fhaUWcUwIlFWEmORY4vUTRfhlrztgQBuaA=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-1fr57gRrTiJL1Mh\/3MrpjLPP9IdJzMiQeobLj5RDXkA=",
+      "url": "js\/background.js"
+    },
+    {
+      "hash": "sha256-b1SuUM50fTqYMAampXG1hHR48F9qixOKWCxLSRLMxxM=",
+      "url": "js\/elementMethods.js"
+    },
+    {
+      "hash": "sha256-nf8iopTVjY0OKHqL6IZVrgKjoxCcViKri5T9iTZKKaU=",
+      "url": "manifest.json"
+    },
+    {
+      "hash": "sha256-MrK1Zz4Qe4UM1uFS\/3hdarGc4Ay54dZi422TgtkmQaU=",
+      "url": "_framework\/_bin\/BlazorApp1.dll"
+    },
+    {
+      "hash": "sha256-peLcTcFwkP9bwlxSMm\/mn5+1BIC2W8beUATuoKCRHeA=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.dll"
+    },
+    {
+      "hash": "sha256-9kpFHp7dCHiOBhg7CcHeRQ6VxYp4BhFTRwzltS5vaPw=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.Web.dll"
+    },
+    {
+      "hash": "sha256-IswrZQytnso\/cp7\/9qOm4JGWbeSc3Np6Ax04eMINHcc=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-jzHgXWAvWMkKIGFjZoT84tbe72E+H7CvTr\/Dryh4QPs=",
+      "url": "_framework\/_bin\/Microsoft.Bcl.AsyncInterfaces.dll"
+    },
+    {
+      "hash": "sha256-WhPC3bWcIpdsZwsEPy3dHHyqmuLqKdo8kvA0GRz2bFE=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-4Z+d7yK140UAWoLA9z2S2XIgWwW7WOzyU\/hmKH9Eo3o=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.Binder.dll"
+    },
+    {
+      "hash": "sha256-C73SEnmh1MlUeOxRtDKU7bDU\/gTiKMvYxemQ1Jn70Vw=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.dll"
+    },
+    {
+      "hash": "sha256-6RRKo2ApBQYFSFJ8WtIJliybJjcA7I77MIKdPzY\/swI=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.Json.dll"
+    },
+    {
+      "hash": "sha256-wAdjLuf\/6D5os2pL4kx7K7Y7hsDENiln3OPywLsC\/a4=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-zuB\/MtEVZ7GMoKGAIrDqH48GZubTuoXRW007Zzpj6t4=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.DependencyInjection.dll"
+    },
+    {
+      "hash": "sha256-2\/g9cqXpakdjfZEa1xWp4Nb\/J\/iWPr\/CIX6t7edVNso=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Logging.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-vs+d15t9hxT34Q9v3RHFmmeW8kh5SXmSUm1vSTsBnA4=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Logging.dll"
+    },
+    {
+      "hash": "sha256-K0iz8n9FJ9UKvZNJU1ECpRxRgmoeWoaTRUaAgw\/HbGc=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Options.dll"
+    },
+    {
+      "hash": "sha256-MgZPs1rPMld5azo0F8gYNunHYhUfiBZPmWvxecbVaNI=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Primitives.dll"
+    },
+    {
+      "hash": "sha256-3cbvJEo8k8VJVQwyEZWWQ6yGRFQW8\/219DaOnmsX3e4=",
+      "url": "_framework\/_bin\/Microsoft.JSInterop.dll"
+    },
+    {
+      "hash": "sha256-zV\/OfJNfNsG404X30Pwkdymdh1KmZpTVEmG\/JNFVc\/k=",
+      "url": "_framework\/_bin\/Microsoft.JSInterop.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-yt1w0jKvCHWzZ9GCyzR\/X4gbJJhMJdu\/fXEWQRgxkIY=",
+      "url": "_framework\/_bin\/mscorlib.dll"
+    },
+    {
+      "hash": "sha256-skO722ucqLqsV5S\/wU9jov\/mwqUALiJ6awknyWFAlHE=",
+      "url": "_framework\/_bin\/System.Core.dll"
+    },
+    {
+      "hash": "sha256-5i2HS\/kQa1g0O7PnSVXQMTlW5n4wogYelyyLFvPNFaU=",
+      "url": "_framework\/_bin\/System.dll"
+    },
+    {
+      "hash": "sha256-d+uP6zdplCwsKGpAVNBl3LABa9kHHTysHsj1k5edzHM=",
+      "url": "_framework\/_bin\/System.Net.Http.dll"
+    },
+    {
+      "hash": "sha256-bEXDY+dnz2ek6+IYjIb8q72BGtbXYZLIQDPOF5fiCL0=",
+      "url": "_framework\/_bin\/System.Net.Http.WebAssemblyHttpHandler.dll"
+    },
+    {
+      "hash": "sha256-UtPPUfnpNTGn1pzSg3wuscavJvF3kX\/ii8QzF8+xJfI=",
+      "url": "_framework\/_bin\/System.Runtime.CompilerServices.Unsafe.dll"
+    },
+    {
+      "hash": "sha256-uFsVIetunNDunQi\/CozGOr2A3fVYyy6cQnE7UYTnji4=",
+      "url": "_framework\/_bin\/System.Text.Encodings.Web.dll"
+    },
+    {
+      "hash": "sha256-Grl7d1KKsIqGV9QnvLxqDLph349iLs6O32iPmN3RxTc=",
+      "url": "_framework\/_bin\/System.Text.Json.dll"
+    },
+    {
+      "hash": "sha256-nXx67eZUfwiKCHLzDG+9mvaIUfWXnwP6IfTK\/QvF4a8=",
+      "url": "_framework\/_bin\/WebAssembly.Bindings.dll"
+    },
+    {
+      "hash": "sha256-mPoqx7XczFHBWk3gRNn0hc9ekG1OvkKY4XiKRY5Mj5U=",
+      "url": "_framework\/wasm\/dotnet.3.2.0.js"
+    },
+    {
+      "hash": "sha256-3S0qzYaBEKOBXarzVLNzNAFXlwJr6nI3lFlYUpQTPH8=",
+      "url": "_framework\/wasm\/dotnet.timezones.dat"
+    },
+    {
+      "hash": "sha256-UC\/3Rm1NkdNdlIrzYARo+dO\/HDlS5mhPxo0IQv7kma8=",
+      "url": "_framework\/wasm\/dotnet.wasm"
+    },
+    {
+      "hash": "sha256-SPHS1EAPAcMFN4TEIUYl3FWtoCQTsNJch0XVGgok1eE=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-wG2k\/LBdVonYjGhEGfF0QfmBoekX4YmBLqeJeJa3S\/w=",
+      "url": "_framework\/blazor.boot.json"
+    }
+  ],
+  "version": "OH5P5231"
+};
